8cf9a3f43c4b508c174adc75c9b40499db2539a6
Alexandra Stoecker <ola.stocker@gmail.com>
2022-04-09 19:58:14 +0200

    finish generate tags

--- js/script.js
+++ js/script.js
@@ -98,25 +98,26 @@ function generateTags(){
   /* START LOOP: for every article: */
   for(let article of articles){
     /* find tags wrapper */
-const generateTags = article.querySelector(optArticleTagsSelector);
+    const generateTags = article.querySelector(optArticleTagsSelector);
     /* make html variable with empty string */
-let html = '';
+    let html = '';
     /* get tags from data-tags attribute */
-const articleTags = article.getAttribute('data-tags');
-console.log(articleTags);
+    const articleTags = article.getAttribute('data-tags');
+    // console.log(articleTags);
     /* split tags into array */
-
+    const articleTagsArray = articleTags.split(' ');
     /* START LOOP: for each tag */
-
+    for (let tag of articleTagsArray){
+    // console.log(tag);
       /* generate HTML of the link */
-
+      let linkHTMLTag = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
       /* add generated code to html variable */
-
+      html.insertAdjacentHTML('beforeend', linkHTMLTag);
     /* END LOOP: for each tag */
-
+    }
     /* insert HTML of all the links into the tags wrapper */
-
+    generateTags.innerHTML = html;
   /* END LOOP: for every article: */
-}
-generateTags();
-};
+  }
+  generateTags();
+
